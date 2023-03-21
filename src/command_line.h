bc5a0227987d3706a1c2f54152bedc970a13d7d2
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-03-21 18:52:11 -0500

    Feature: Implement logic for command line parsing

--- src/command_line.h
+++ src/command_line.h
@@ -55,7 +55,79 @@ char *get_value(char short_name) {
   return NULL;
 }
 
+void usage() {
+  printf("Usage:\n");
+  for (int i = 0; i < 26; i++) {
+    if (Arguments[i].description) {
+      printf("  -%c,--%s ", 'a' + i, Arguments[i].long_name);
+      for (int j = strlen(Arguments[i].long_name); j < 17; j++) {
+        printf(" ");
+      }
+      printf("%s\n", Arguments[i].description);
+    }
+  }
+
+  exit(EXIT_SUCCESS);
+}
+
 void parse_opts(int argc, char *argv[]) {
+  add_arg('h', "help", "Display Usage statement.");
+  set_arg_function(usage, 'h');
+
+  int last_arg = -1;
+
+  for (int i = 1; i < argc; i++) {
+    if (strlen(argv[i]) >= 2) {
+      if (argv[i][0] == '-') {
+
+        // Short opts
+        if (argv[i][1] != '-') {
+          for (int j = 1; j < strlen(argv[i]); j++) {
+            last_arg = argv[i][j] - 'a';
+            Arguments[last_arg].set = 1;
+            if (Arguments[last_arg].function) {
+              Arguments[last_arg].function();
+            }
+          }
+
+          // Long opts
+        } else {
+          if (strlen(argv[i]) >= 3) {
+            for (int j = 0; j < 26; j++) {
+              if (Arguments[j].long_name) {
+                if (strcmp(Arguments[j].long_name, argv[i] + 2) == 0) {
+                  last_arg = j;
+                  Arguments[last_arg].set = 1;
+                  if (Arguments[last_arg].function) {
+                    Arguments[last_arg].function();
+                  }
+                  break;
+                }
+              }
+            }
+
+            // Double hyphen
+          } else {
+            return;
+          }
+        }
+
+        // Value
+      } else {
+        if (last_arg != -1) {
+          Arguments[last_arg].value = (char *)malloc(strlen(argv[i]) + 1);
+          strcpy(Arguments[last_arg].value, argv[i]);
+        }
+      }
+
+      // Single character arg
+    } else {
+      if (last_arg != -1) {
+        Arguments[last_arg].value = (char *)malloc(strlen(argv[i]) + 1);
+        strcpy(Arguments[last_arg].value, argv[i]);
+      }
+    }
+  }
 }
 
 #endif
